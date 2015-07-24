1c0ef08825715feb0d6e76fabee167dae7b0ab36
sue445 <sue445@sue445.net>
2015-07-25 01:52:02 +0900

    Add country list

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 
 ruby "2.2.2"
 
+gem "activesupport"
 gem "thin"
 gem "sinatra"
 gem "slim"
--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require "sinatra"
 require "slim"
+require "yaml"
+require "active_support/all"
 
-get "/" do
-  slim :index
+def load_countries
+  YAML.load_file("#{__dir__}/config/countries.yml").map(&:with_indifferent_access).sort_by { |c| c[:name] }
 end
 
+get "/" do
+  @countries = load_countries
+  slim :index
+end
