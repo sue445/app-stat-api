162b6d60e7012fe5ccb4f7f9fd3699446658dadd
sue445 <sue445@sue445.net>
2015-07-27 01:25:50 +0900

    Refactor: use helpers block

--- app.rb
+++ app.rb
@@ -6,32 +6,34 @@ require "active_support/all"
 require "apple_system_status"
 require "dalli"
 
-def load_countries
-  YAML.load_file("#{__dir__}/config/countries.yml").map(&:with_indifferent_access).sort_by { |c| c[:name] }
-end
+helpers do
+  def load_countries
+    YAML.load_file("#{__dir__}/config/countries.yml").map(&:with_indifferent_access).sort_by { |c| c[:name] }
+  end
 
-def fetch_apple_system_status(country)
-  cache = cache_client
+  def fetch_apple_system_status(country)
+    cache = cache_client
 
-  cached_status = cache.get(country)
-  return cached_status if cached_status
+    cached_status = cache.get(country)
+    return cached_status if cached_status
 
-  system_status = AppleSystemStatus::Crawler.new.perform(country: country)
-  cache.set(country, system_status)
-  system_status
-end
+    system_status = AppleSystemStatus::Crawler.new.perform(country: country)
+    cache.set(country, system_status)
+    system_status
+  end
 
-def apple_system_status(country, title)
-  system_status = fetch_apple_system_status(country)
-  return system_status if title.blank?
+  def apple_system_status(country, title)
+    system_status = fetch_apple_system_status(country)
+    return system_status if title.blank?
 
-  system_status[:services].select! { |service| service[:title] == title }
-  system_status
-end
+    system_status[:services].select! { |service| service[:title] == title }
+    system_status
+  end
 
-def cache_client
-  # TODO: heroku memcached
-  Dalli::Client.new("localhost:11211", namespace: "apple_system_status", compress: true, expires_in: 5.minutes)
+  def cache_client
+    # TODO: heroku memcached
+    Dalli::Client.new("localhost:11211", namespace: "apple_system_status", compress: true, expires_in: 5.minutes)
+  end
 end
 
 get "/" do
