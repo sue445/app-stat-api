b966d31f40471727c6f264b44ce5e5265bb24b6f
sue445 <sue445@sue445.net>
2016-09-04 09:33:07 +0900

    Use enable_rolling_restart
    https://github.com/schneems/puma_worker_killer#turn-on-rolling-restarts

--- config/puma.rb
+++ config/puma.rb
@@ -118,13 +118,8 @@ environment ENV["RACK_ENV"] || "development"
 # end
 
 before_fork do
-  PumaWorkerKiller.config do |config|
-    config.ram           = 512 # mb
-    config.frequency     = 5    # seconds
-    config.percent_usage = 0.98
-    config.rolling_restart_frequency = 12 * 3600 # 12 hours in seconds
-  end
-  PumaWorkerKiller.start
+  require "puma_worker_killer"
+  PumaWorkerKiller.enable_rolling_restart(6 * 3600)
 end
 
 # Code to run in a worker before it starts serving requests.
