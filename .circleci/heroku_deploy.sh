039bbe4065921fd3edb89e0d2d5a3484d02defc6
sue445 <sue445@sue445.net>
2018-02-12 18:53:06 +0900

    git push with retry

--- .circleci/heroku_deploy.sh
+++ .circleci/heroku_deploy.sh
@@ -9,6 +9,6 @@ heroku config:add BUNDLE_WITHOUT="test:development" --app $HEROKU_APP_NAME
 # Please make sure you have the correct access rights
 # and the repository exists.
 # Exited with code 128
-sleep 5
+. ./.circleci/retty.sh
 
-git push git@heroku.com:$HEROKU_APP_NAME.git $CIRCLE_SHA1:refs/heads/master
+retry 3 git push git@heroku.com:$HEROKU_APP_NAME.git $CIRCLE_SHA1:refs/heads/master
